40de90c496f7d52ea35695f33a62028afb4ad179
Oleksii-Sumy <axpert17@gmail.com>
2022-10-31 12:16:11 +0200

    ch

--- js/array/index.js
+++ js/array/index.js
@@ -5,25 +5,28 @@
 // 3. Write draft solution & testing
 // 4. Refactoring & final testing -> final solution
 
-function getPrimes (num) {
-  console.log ('NUM:', num);
-for (let number = 2; number <= num; number += 1) {
-let counter = 0;
-for (let index = 2; index <= number; index +=1) {
-if (number & index === 0) {
-  counter += 1;
-}
-}
-if ( counter == 2) {
-console.log (number);
-}
-}
+function getPrimes(num) {
+  // console.log('NUM:', num);   for testing
+
+  for (let number = 2; number <= num; number += 1) {
+    // console.log ('CHECKING IF NUMBER IS PRIME' + number);  for testing
+    let counter = 0;
+
+    for (let index = 1; index <= number; index += 1) {
+      if (number % index === 0) {
+        counter += 1;
+      }
+    }
+    // console.log('For number ' + number + ' counter find ' + counter);  for testing
+    if (counter == 2) {
+      console.log(number);
+    }
+  }
 }
 //test data
 getPrimes(9);
 // getPrimes(11);
 // getPrimes(16);
-
 
 /// input 10
 // autput:
@@ -32,23 +35,13 @@ getPrimes(9);
 // 5
 // 7
 
-
-
 //// algoritm:
 
 // a)  Not best variant:
-// 1. iterate from 1 to num 
+// 1. iterate from 1 to num
 // 2. check if num is prime
-// 2.0 create CountQueuingStrategy 
+// 2.0 create CountQueuingStrategy
 // 2.1 itarate 1..number
 // 2.2 if number % index === 0 -> count ++
 // 2.3 if coount >= 2  -> is not prime
 // 3. if prime -> console.log
-
-
-
-
-
-
-
-
